151e3506c591971e8495f0f6f59cd2f35332b303
rtesini <rtesini@gmail.com>
2017-10-20 14:02:36 -0200

    user-to and tests

--- tests.sh
+++ tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -xe
 python -m SimpleHTTPServer 8000 &
+FOO_PID=$!
 
 # parametro -s é o diretorio
 # parametro -p é uma regex para dizer quais arquivos procurar
 python -m unittest discover -s test -p "*Test*"
+
+kill $FOO_PID
